c9ed208d1c4c5bd5c2dd5b33fa858cf828bded9c
Ferrowrafael <ferrowrafael@gmail.com>
2020-08-26 21:20:27 +0200

    Fixing task controller

--- todoList-MongoDB/app.js
+++ todoList-MongoDB/app.js
@@ -1,9 +1,24 @@
 let express = require('express');
+const mongoose = require('mongoose');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let app = express();
 
+const cors = require('./middleware/cors.js');
+
+// CONNECT DATA BASE
+mongoose.connect('mongodb://localhost:27017/TodoListMongo', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('Successfully connected to MongoDB'))
+    .catch(console.error);
+    
+
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
