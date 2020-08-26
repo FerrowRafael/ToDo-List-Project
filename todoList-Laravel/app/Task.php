c393fe3b23900418570a3e42fc8d60f12257f6dd
Ferrowrafael <ferrowrafael@gmail.com>
2020-08-26 22:25:26 +0200

    Creating tables

--- todoList-Laravel/app/Task.php
+++ todoList-Laravel/app/Task.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable=[
+        'name',
+        'description',
+        'status_id',
+        'priority_id',
+        'category_id'
+    ];    
+
+    // RELACIONES
+    // public function user()
+    // {
+    //    return $this->belongsTo('\App\User');
+    // }
 }
