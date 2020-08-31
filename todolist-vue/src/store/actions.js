2b0f40b244d4b4560d4d7c407ad113770fe9a8a1
Ferrowrafael <ferrowrafael@gmail.com>
2020-08-31 14:01:49 +0200

    Vue: store almost complete

--- todolist-vue/src/store/actions.js
+++ todolist-vue/src/store/actions.js
@@ -4,11 +4,37 @@ const API_URL = 'http://localhost:8000/api/tasks';
 export default {
 
     GET_TASKS ({ commit }) {
-        console.log("PLatano")
         axios.get(`${API_URL}`)
         .then((res) => {
             if (res.status === 200) {
                 commit('SET_TASKS', res.data);
+            }
+        })
+    },
+
+    CREATE_TASK ({ commit }) {
+        axios.post(`${API_URL}`)
+        .then((res) => {
+            if (res.status === 200) {
+                commit('ADD_TASK', res.data);
+            }
+        })
+    },
+
+    UPDATE_TASKS ({ commit }, id) {
+        axios.put(`${API_URL}/${id}`)
+        .then((res) => {
+            if (res.status === 200) {
+                commit('UPDATE_TASK', res.data);
+            }
+        })
+    },
+
+    DELETE_TASK ({ commit }, id) {
+        axios.delete(`${API_URL}/${id}`)
+        .then((res) => {
+            if (res.status === 200) {
+                commit('DELETE_TASK', res.data);
             }
         })
     },
--- todolist-vue/src/store/mutations.js
+++ todolist-vue/src/store/mutations.js
@@ -4,5 +4,17 @@ export default {
         state.tasks = tasks;
     },
 
+    CREATE_TASK (state, tasks) {
+        state.tasks = tasks;
+    },
+
+    UPDATE_TASKS (state, tasks) {
+        state.tasks = tasks;
+    },
+
+    DELETE_TASK (state, tasks) {
+        state.tasks = tasks;
+    },
+
 
 }
