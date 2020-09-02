7580da2aadd274a2b875af86cc03e1ffecfe9e8f
Ferrowrafael <ferrowrafael@gmail.com>
2020-09-02 23:32:11 +0200

    React: Improve update and adding css

--- todoList-React/src/components/TodoListItem/TodoListItem.jsx
+++ todoList-React/src/components/TodoListItem/TodoListItem.jsx
@@ -8,7 +8,7 @@ const TodoListItem = ({ task }) => {
     const [showResults, setShowResults] = useState(false)
     const onClick = () => setShowResults(true)
 
-    const delete_Task = (id) => {
+    const removeTask = (id) => {
         console.log(id)
         deleteTask(id)
         allTasks()
@@ -16,46 +16,46 @@ const TodoListItem = ({ task }) => {
 
     return (
         <Fragment >
-            <div className="task" key={task?.id}>
+            {/* <div className="task" key={task?.id}>
                 <p>{task.name}</p>
                 <input type="submit" value="Search" onClick={onClick} />
-                {/* <button onClick={()=>editTask(i)}><i class="fas fa-edit"></i></button> */}
-                <button onClick={()=>delete_Task(task.id)}><i class="fas fa-trash-alt"></i></button>
-            </div>
+                <button onClick={()=>editTask(i)}><i className="fas fa-edit"></i></button>
+                <button onClick={()=>delete_Task(task.id)}><i className="fas fa-trash-alt"></i></button>
+            </div> */}
 
-            <div class="card">
-                    <div class="card__content">
-                        <div class="card__title">{task.name}</div> 
+            <div className="card">
+                    <div className="card__content">
+                        <div className="card__title">{task.name}</div> 
                         <div>
-                            <input type="checkbox" class="checkComplete" id="complete" name="complete" value="Complete" 
+                            <input type="checkbox" className="checkComplete" id="complete" name="complete" value="Complete" 
                             // v-model="task.status" click="checkboxToggle(category)"
                             />  
-                            <label for="complete"> Complete </label><br/>  
+                            <label> Complete </label><br/>  
                         </div>
                                        
-                        <div class="btns">
-                            {/* <button class="btn btn--block" v-on:click="showUpdate"><i class="fas fa-edit"></i></button>
-                            <button class="btn btn--block" v-on:click="removeTask(task._id)"><i class="fas fa-trash-alt"></i></button> */}
+                        <div className="btns">
+                            <button className="btn btn--block" onClick="showUpdate"><i className="fas fa-edit"></i></button>
+                            <button className="btn btn--block" onClick="removeTask(task._id)"><i className="fas fa-trash-alt"></i></button>
                         </div>
 
-                        <div class='status completed' v-show="task.status" disabled>
-                            Completed
+                        <div className='status completed' style={{ display: !task.status ? "block" : "none" }} disabled>
+                            Completed 
                         </div>
-                        <div class='status pending' v-show="!task.status">
+                        <div className='status pending' style={{ display: task.status ? "block" : "none" }} show="!task.status">
                             Pending
                         </div>
                     </div>
                 </div>
 
             {/* UPDATE TASK  */}
-                {/* <div class="update-form">
-                    <div class="form-group">
+                {/* <div className="update-form">
+                    <div className="form-group">
                             <label for="title">Title</label>
-                            <input type="text" class="form-control" id="name"
+                            <input type="text" className="form-control" id="name"
                             required name="name"
                             />
                     </div>
-                    <button on-click="updateTask()" class="btn btn-success">Submit</button>     
+                    <button on-click="updateTask()" className="btn btn-success">Submit</button>     
                 </div> */}  
         </Fragment>
     )    
