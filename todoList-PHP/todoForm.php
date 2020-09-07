bdd4904a4e27fa65329a702c924d0d92ca643179
Ferrowrafael <ferrowrafael@gmail.com>
2020-09-07 22:01:05 +0200

    PHP: Starting PHP project

---
+++ todoList-PHP/todoForm.php
@@ -0,0 +1,26 @@
+<?php
+require_once "index.php";
+?>
+<html>
+<body>
+<form action='administrar_libro.php' method='post'>
+	<table>
+		<tr>
+			<td>Nombre libro:</td>
+			<td> <input type='text' name='nombre'></td>
+		</tr>
+		<tr>
+			<td>Autor:</td>
+			<td><input type='text' name='autor' ></td>
+		</tr>
+		<tr>
+			<td>Fecha Edición:</td>
+			<td><input type='text' name='edicion' ></td>
+		</tr>
+		<input type='hidden' name='insertar' value='insertar'>
+	</table>
+	<input type='submit' value='Guardar'>
+	<a href="index.php">Volver</a>
+</form>
+</body>
+</html>
